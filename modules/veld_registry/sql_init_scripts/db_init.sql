9e847b94685ae04925613d25f7126c0c32f471bd
steffres <stefan.resch@oeaw.ac.at>
2023-09-15 17:25:00 +0200

    remove insert statements from sql init

--- modules/veld_registry/sql_init_scripts/db_init.sql
+++ modules/veld_registry/sql_init_scripts/db_init.sql
@@ -12,7 +12,3 @@ CREATE TABLE veld (
     veld_repo_id INTEGER NOT NULL,
     CONSTRAINT fk_veld_repo_id FOREIGN KEY (veld_repo_id) REFERENCES veld_repo (id)
 );
-
-INSERT INTO veld_repo (local_path) VALUES ('path_a');
-INSERT INTO veld (commit, veld_repo_id) VALUES ('commit_a', 1);
-INSERT INTO veld (commit, veld_repo_id) VALUES ('commit_b', 1);
